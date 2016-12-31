9ee7387b526056f79d2f6025193887beb4950baf
dubcanada <dubcanada@gmail.com>
2016-12-31 17:55:42 -0400

    Remove validation on options
    Request will validate the options in their library, there is no reason we need to revalidate them here.

--- src/main.js
+++ src/main.js
@@ -9,12 +9,8 @@ request.debug = false;
 Arequest = (defaultOptions) => {
     let arequest;
 
-    Arequest.validateOptions(defaultOptions);
-
     arequest = async (url, options) => {
         return new Promise((resolve) => {
-            Arequest.validateOptions(options);
-
             options = _.assign({url: url}, options, defaultOptions);
 
             options = Arequest.mapOptions(options);
@@ -46,27 +42,6 @@ Arequest = (defaultOptions) => {
     };
 
     return arequest;
-};
-
-/**
- *
- */
-Arequest.validateOptions = (options) => {
-    let unknownOption;
-
-    if (!options) {
-        return;
-    }
-
-    unknownOption = _.first(_.difference(_.keys(options), ['method', 'data', 'headers', 'proxy', 'cookieJar', 'cookieJar2']));
-
-    if (unknownOption) {
-        throw new Error('Unknown option ("' + unknownOption + '").');
-    }
-
-    if (options.method && _.indexOf(['GET', 'POST', 'PUT', 'HEAD', 'DELETE'], options.method) === -1) {
-        throw new Error('Unknown option.method value ("' + options.method + '").');
-    }
 };
 
 /**
